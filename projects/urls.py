39d0327dd64524d922a0c137d53a94620b0b445a
Selva Kalusalingam R <selvakalusu003@gmail.com>
2026-03-05 13:48:25 +0530

    Add temporary admin creation endpoint

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProjectViewSet, TaskViewSet, RegisterView
+from .views import ProjectViewSet, TaskViewSet, RegisterView, create_admin
 from django.http import JsonResponse
 
 def home(request):
@@ -15,6 +15,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('rest_framework.urls')),
     path('', home),
+    path('create-admin/', create_admin),
 ]
 
 
--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,9 @@ from rest_framework import viewsets, generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Project, Task
 from .serializers import ProjectSerializer, TaskSerializer, RegisterSerializer
-
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
@@ -32,3 +34,14 @@ class TaskViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+@api_view(['GET'])
+def create_admin(request):
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@test.com",
+            password="admin123"
+        )
+        return Response({"message": "Admin user created"})
+    return Response({"message": "Admin already exists"})
